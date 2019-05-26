b1fb1b70a611aba9edec8702934c052fcba6ab12
Gilad Barnea <giladbrn@gmail.com>
2019-05-26 14:11:15 +0300

    added tsconfig.json, mainjs, maints

---
+++ main/main.js
@@ -0,0 +1,21 @@
+class Logger {
+    constructor(options) {
+        const { name, age } = options;
+        this._name = name;
+        this._age = age;
+        console.log(Object.assign({}, options, { msg: "hey" }));
+    }
+    get name() {
+        return this._name;
+    }
+    set name(val) {
+        this._name = val;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(val) {
+        this._age = val;
+    }
+}
+const logger = new Logger({ name: "gilad", age: 27 });
---
+++ main/main.ts
@@ -0,0 +1,25 @@
+class Logger {
+	_name: string;
+	_age: string;
+
+	constructor(options) {
+		const { name, age } = options;
+		this._name = name;
+		this._age = age;
+		console.log({ ...options, msg: "hey" })
+	}
+	get name() {
+		return this._name;
+	}
+	set name(val) {
+		this._name = val;
+	}
+	get age() {
+		return this._age;
+	}
+	set age(val) {
+		this._age = val;
+	}
+}
+
+const logger = new Logger({ name: "gilad", age: 27 });
