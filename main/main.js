94e4487ba2b4497de2aeb85bbcb2407e6a912a9b
Gilad Barnea <giladbrn@gmail.com>
2019-06-19 16:46:30 +0300

    got caoursel

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
 const carouselRight = document.querySelector("body > main > carousel > button.right");
 const carouselLeft = document.querySelector("body > main > carousel > button.left");
 const isIphone = window.clientInformation.userAgent.includes('iPhone');
+const siema = new Siema({
+    selector: '.siema',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: false,
+    rtl: false,
+    onInit: () => {
+    },
+    onChange: () => {
+    },
+});
+console.log({ siema });
 //# sourceMappingURL=main.js.map
--- main/main.ts
+++ main/main.ts
@@ -45,3 +45,20 @@ const carouselLeft = document.querySelector("body > main > carousel > button.lef
 
 const isIphone = window.clientInformation.userAgent.includes('iPhone');
 
+const siema = new Siema({
+    selector: '.siema',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: false,
+    rtl: false,
+    onInit: () => {
+    },
+    onChange: () => {
+    },
+});
+console.log({siema});
