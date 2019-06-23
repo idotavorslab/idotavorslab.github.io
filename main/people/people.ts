a534f828059e825c7192a3521a0123a40cf882f9
Gilad Barnea <giladbrn@gmail.com>
2019-06-23 20:38:48 +0300

    people container

---
+++ main/people/people.ts
@@ -0,0 +1,30 @@
+const PeoplePage = () => {
+    async function init() {
+        
+        console.log('PeoplePage init');
+        home.empty();
+        elem({id: 'page_css'}).attr({href: 'main/people/people.css'});
+        let req = new Request('main/people/people.json', {cache: "no-cache"});
+        const data = await (await fetch(req)).json();
+        console.log(data);
+        const people = [];
+        for (let [name, {img, role}] of dict(data).items()) {
+            let person = elem({tag: "person"});
+            person
+                .append(
+                    div({text: name, cls: "name"}),
+                    div({text: role, cls: "role"}),
+                )
+                .css({backgroundImage: `url("main/people/${img}")`});
+            people.push(person);
+        }
+        const peopleContainer = div({id: "people_container"})
+            .append(...people);
+        home.append(peopleContainer);
+        
+        
+    }
+    
+    
+    return {init}
+};
--- main/research/research.ts
+++ main/research/research.ts
@@ -1,18 +1,18 @@
 const ResearchPage = () => {
     async function init() {
         
-        console.log('hi from ResearchPage');
-        let myRequest = new Request('main/research/research.json', {cache: "no-cache"});
-        const data = await (await fetch(myRequest)).json();
-        console.log(data);
+        console.log('ResearchPage init');
         home.empty();
         elem({id: 'page_css'}).attr({href: 'main/research/research.css'});
+        let req = new Request('main/research/research.json', {cache: "no-cache"});
+        const data = await (await fetch(req)).json();
+        console.log(data);
         for (let [title, {img, text}] of dict(data).items()) {
             let article = elem({tag: "article"});
             article
                 .append(
                     div({text: title, cls: "title"}),
-                    div({text, cls: "text"}),
+                    div({text: text, cls: "text"}),
                 )
                 .css({backgroundImage: `url("main/research/${img}")`});
             home.append(article);
