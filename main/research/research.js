6784cf5f89f77128ab305a2cda916d5099dfa6d5
Gilad Barnea <giladbrn@gmail.com>
2019-06-23 14:30:37 +0300

    can now read json file

--- main/research/research.js
+++ main/research/research.js
@@ -1,21 +1,9 @@
 const ResearchPage = () => {
     async function sayHi() {
         console.log('hi from ResearchPage');
-        let myRequest = new Request('main/research/research.json', {
-            method: "GET",
-            headers: { 'Content-Type': 'text/json' }
-        });
-        fetch(myRequest)
-            .then(function (response) {
-            if (!response.ok) {
-                throw new Error('HTTP error, status = ' + response.status);
-            }
-            debugger;
-            return response.blob();
-        })
-            .then(function (response) {
-            debugger;
-        });
+        let myRequest = new Request('main/research/research.json');
+        const data = await (await fetch(myRequest)).json();
+        console.log(data);
     }
     return { sayHi };
 };
--- main/research/research.ts
+++ main/research/research.ts
@@ -2,33 +2,10 @@ const ResearchPage = () => {
     async function sayHi() {
         
         console.log('hi from ResearchPage');
-        let myRequest = new Request('main/research/research.json', {
-            method: "GET",
-            // credentials: "same-origin",
-            // mode: "cors",
-            headers: {'Content-Type': 'text/json'}
-        });
-        /* let myHeaders = new Headers();
-         myHeaders.append('Content-Type', 'text/json');
-         
-         const myInit = {
-             method: 'GET',
-             headers: myHeaders,
-             mode: 'cors',
-             cache: 'default'
-         };
-        */
-        fetch(myRequest)
-            .then(function (response) {
-                if (!response.ok) {
-                    throw new Error('HTTP error, status = ' + response.status);
-                }
-                debugger;
-                return response.blob();
-            })
-            .then(function (response) {
-                debugger;
-            });
+        let myRequest = new Request('main/research/research.json');
+        const data = await (await fetch(myRequest)).json();
+        console.log(data);
+        
         
     }
     
