6cdc5be6387d297248c46bfe0b7f74b2d5f01b81
Gilad Barnea <giladbrn@gmail.com>
2019-06-28 13:26:03 +0300

    compressed css files

--- common/js/typings.ts
+++ common/js/typings.ts
@@ -1,66 +1,58 @@
 type TEvent = keyof HTMLElementEventMap;
-type TEventFunctionMap<K> = { [P in Extract<K, string>]?: (evt: Event) => void }
+type TEventFunctionMap<K> = {
+	[P in Extract<K, string>]?: (evt: Event) => void
+};
 
-
-type TMap<T> = { [s: string]: T }
-type TMap2<T, K extends keyof T> = { [s: string]: T[K] }
-type TMap4<T> = { [s: string]: T[keyof T] }
-type TMap3<T> = { [P in keyof T]: T[P] }
-type TKeyValue<K, V> = { [S in Extract<K, string>]: V }
+type TMap<T> = { [s: string]: T };
 
 type TElemOptions = {
-    tag?: 'span' | 'div' | 'button' | 'img' | any,
-    id?: string,
-    text?: string,
-    htmlElement?: HTMLElement,
-    query?: string,
-    children?: TMap<string>,
-    cls?: string
-}
-
+	tag?: "span" | "div" | "button" | "img" | any;
+	id?: string;
+	text?: string;
+	htmlElement?: HTMLElement;
+	query?: string;
+	children?: TMap<string>;
+	cls?: string;
+};
 
 type TSubElemOptions = {
-    id?: string,
-    text?: string,
-    cls?: string
+	id?: string;
+	text?: string;
+	cls?: string;
 };
 type TImgOptions = {
-    id?: string,
-    src?: string,
-    cls?: string
+	id?: string;
+	src?: string;
+	cls?: string;
 };
 
-
 type TElemAttrs = {
-    src?: string,
-    href?: string,
-}
+	src?: string;
+	href?: string;
+};
 
 interface TElemCssOpts {
-    animation?: string,
-    backgroundColor?: string,
-    backgroundImage?: string,
-    boxShadow?: string,
-    color?: string,
-    display?: string,
-    filter?: string,
-    fontSize?: string,
-    gridColumn?: string,
-    gridRow?: string,
-    gridTemplateColumns?: string,
-    height?: string | number,
-    letterSpacing?: string,
-    marginRight?: string,
-    opacity?: number,
-    zIndex?: number,
-    
+	animation?: string;
+	backgroundColor?: string;
+	backgroundImage?: string;
+	boxShadow?: string;
+	color?: string;
+	display?: string;
+	filter?: string;
+	fontSize?: string;
+	gridColumn?: string;
+	gridRow?: string;
+	gridTemplateColumns?: string;
+	height?: string | number;
+	letterSpacing?: string;
+	marginRight?: string;
+	opacity?: number;
+	zIndex?: number;
 }
 
 type StringOrNumber = string | number;
 
 type TAjax = {
-    post: (url: string, data: any) => Promise<any>;
-    get: (url: string) => Promise<any>
-}
-
-
+	post: (url: string, data: any) => Promise<any>;
+	get: (url: string) => Promise<any>;
+};
