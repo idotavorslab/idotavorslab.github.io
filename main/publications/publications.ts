1155d42e749c5e10aa3cc3e7d67b95f9871cc448
Gilad Barnea <giladbrn@gmail.com>
2019-06-29 18:40:34 +0300

    some publications css

--- main/publications/publications.ts
+++ main/publications/publications.ts
@@ -2,29 +2,27 @@ const PublicationsPage = () => {
     async function init() {
         
         console.log('PublicationsPage init');
-        Home.empty();
-        // elem({id: 'page_css'}).attr({href: 'main/publications/publications.css'});
         let req = new Request('main/publications/publications.json', {cache: "no-cache"});
         const data = await (await fetch(req)).json();
         console.log(data);
         const papers = [];
         for (let [title, {year, creds, mag, link}] of dict(data).items()) {
             let paper = elem({tag: "paper"});
-            paper
-                .append(
-                    div({text: creds, cls: "creds"}),
-                    div({text: year, cls: "year"}),
-                    div({text: title, cls: "title"}),
-                    div({text: mag, cls: "mag"}),
-                    elem({tag: 'a', text: 'LINK'}).attr({href: link}),
-                );
+            paper.cacheAppend({
+                title: div({text: title, cls: "title"}),
+                creds: span({text: creds, cls: "creds"}),
+                year: span({text: ` (${year})`, cls: "year"}),
+                mag: div({text: mag, cls: "mag"}),
+            }).pointerdown(() => {
+                window.open(link)
+            });
             
             papers.push(paper);
             
         }
         const papersContainer = div({id: "papers_container"})
             .append(...papers);
-        Home.append(papersContainer);
+        Home.empty().append(papersContainer);
         
         
     }
