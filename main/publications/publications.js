cb169fc726adf980216e7887fea34aadaa52a871
giladbarnea <giladbrn@gmail.com>
2019-08-13 14:53:02 +0300

    publication papers now sorted by year

--- main/publications/publications.js
+++ main/publications/publications.js
@@ -1,35 +1,41 @@
 const PublicationsPage = () => {
-    async function init() {
-        console.log('PublicationsPage init');
-        Navbar.select(Navbar.publications);
-        let req = new Request('main/publications/publications.json', { cache: "no-cache" });
-        const data = await (await fetch(req)).json();
-        console.log(data);
-        const papers = [];
-        for (let [title, { year, creds, mag, thumbnail, link }] of dict(data).items()) {
-            let paper = elem({ tag: "paper" });
-            function openLink() {
+    class Paper {
+        constructor(title, year, creds, mag, thumbnail, link) {
+            function _openLink() {
                 if (link.includes('http') || link.includes('www'))
                     window.open(link);
                 else
                     window.open(`main/publications/${link}`);
             }
-            paper.cacheAppend({
-                thumb: img({ src: `main/publications/${thumbnail}`, cls: "thumbnail" }).pointerdown(openLink),
+            this.elem = elem({ tag: "paper" })
+                .cacheAppend({
+                thumb: img({ src: `main/publications/${thumbnail}`, cls: "thumbnail" }).pointerdown(_openLink),
                 content: div({ cls: "content-div" }).cacheAppend({
-                    title: div({ text: title, cls: "title" }).pointerdown(openLink),
+                    title: div({ text: title, cls: "title" }).pointerdown(_openLink),
                     creds: span({ text: creds, cls: "creds" }),
                     year: span({ text: ` (${year})`, cls: "year" }),
                     mag: div({ text: mag, cls: "mag" })
                 }),
                 pdf: div({ cls: 'pdf-div' })
-                    .pointerdown(openLink)
+                    .pointerdown(_openLink)
                     .text(link.split('.').reverse()[0].toUpperCase())
             });
-            papers.push(paper);
+            this.year = year;
         }
+    }
+    async function init() {
+        console.log('PublicationsPage init');
+        Navbar.select(Navbar.publications);
+        let req = new Request('main/publications/publications.json', { cache: "no-cache" });
+        const data = await (await fetch(req)).json();
+        console.log('PublicationsPage data:', data);
+        const papers = [];
+        for (let [title, { year, creds, mag, thumbnail, link }] of dict(data).items()) {
+            papers.push(new Paper(title, year, creds, mag, thumbnail, link));
+        }
+        papers.sort((a, b) => b.year - a.year);
         const papersContainer = div({ id: "papers_container" })
-            .append(...papers);
+            .append(...papers.map(p => p.elem));
         Home.empty().append(papersContainer);
     }
     return { init };
--- main/publications/publications.ts
+++ main/publications/publications.ts
@@ -1,41 +1,73 @@
 const PublicationsPage = () => {
-    async function init() {
+    class Paper {
+        elem: BetterHTMLElement;
+        year: number;
         
-        console.log('PublicationsPage init');
-        Navbar.select(Navbar.publications);
-        let req = new Request('main/publications/publications.json', {cache: "no-cache"});
-        const data = await (await fetch(req)).json();
-        console.log(data);
-        const papers = [];
-        for (let [title, {year, creds, mag, thumbnail, link}] of dict(data).items()) {
-            let paper = elem({tag: "paper"});
-            
-            function openLink() {
+        constructor(title: string, year: number, creds: string, mag: string, thumbnail: string, link: string) {
+            function _openLink() {
                 if (link.includes('http') || link.includes('www'))
                     window.open(link);
                 else // local
                     window.open(`main/publications/${link}`)
             }
             
-            paper.cacheAppend({
-                thumb: img({src: `main/publications/${thumbnail}`, cls: "thumbnail"}).pointerdown(openLink),
-                content: div({cls: "content-div"}).cacheAppend({
-                    title: div({text: title, cls: "title"}).pointerdown(openLink),
-                    creds: span({text: creds, cls: "creds"}),
-                    year: span({text: ` (${year})`, cls: "year"}),
-                    mag: div({text: mag, cls: "mag"})
-                }),
-                pdf: div({cls: 'pdf-div'})
-                    .pointerdown(openLink)
-                    .text(link.split('.').reverse()[0].toUpperCase()) // ext
-                
-            });
+            this.elem = elem({tag: "paper"})
+                .cacheAppend({
+                    thumb: img({src: `main/publications/${thumbnail}`, cls: "thumbnail"}).pointerdown(_openLink),
+                    content: div({cls: "content-div"}).cacheAppend({
+                        title: div({text: title, cls: "title"}).pointerdown(_openLink),
+                        creds: span({text: creds, cls: "creds"}),
+                        year: span({text: ` (${year})`, cls: "year"}),
+                        mag: div({text: mag, cls: "mag"})
+                    }),
+                    pdf: div({cls: 'pdf-div'})
+                        .pointerdown(_openLink)
+                        .text(link.split('.').reverse()[0].toUpperCase()) // ext
+                    
+                });
+            this.year = year;
+        }
+    }
+    
+    async function init() {
+        
+        console.log('PublicationsPage init');
+        Navbar.select(Navbar.publications);
+        let req = new Request('main/publications/publications.json', {cache: "no-cache"});
+        const data = await (await fetch(req)).json();
+        console.log('PublicationsPage data:', data);
+        const papers: Paper[] = [];
+        for (let [title, {year, creds, mag, thumbnail, link}] of dict(data).items()) {
             
-            papers.push(paper);
+            
+            // function openLink() {
+            //     if (link.includes('http') || link.includes('www'))
+            //         window.open(link);
+            //     else // local
+            //         window.open(`main/publications/${link}`)
+            // }
+            //
+            // let paper = elem({tag: "paper"})
+            //     .cacheAppend({
+            //         thumb: img({src: `main/publications/${thumbnail}`, cls: "thumbnail"}).pointerdown(openLink),
+            //         content: div({cls: "content-div"}).cacheAppend({
+            //             title: div({text: title, cls: "title"}).pointerdown(openLink),
+            //             creds: span({text: creds, cls: "creds"}),
+            //             year: span({text: ` (${year})`, cls: "year"}),
+            //             mag: div({text: mag, cls: "mag"})
+            //         }),
+            //         pdf: div({cls: 'pdf-div'})
+            //             .pointerdown(openLink)
+            //             .text(link.split('.').reverse()[0].toUpperCase()) // ext
+            //
+            //     });
+            
+            papers.push(new Paper(title, year, creds, mag, thumbnail, link));
             
         }
+        papers.sort((a, b) => b.year - a.year);
         const papersContainer = div({id: "papers_container"})
-            .append(...papers);
+            .append(...papers.map(p => p.elem));
         Home.empty().append(papersContainer);
         
         
