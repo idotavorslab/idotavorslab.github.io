c8fa7f63b60009b612f0d6f5f159abfb5c250746
giladbarnea <giladbrn@gmail.com>
2019-08-14 12:45:59 +0300

    linearGradient separators with TL

--- main/navbar/navbar.ts
+++ main/navbar/navbar.ts
@@ -29,15 +29,28 @@ const Navbar = <INavbar>elem({
     }
 });
 
-Navbar.select = child => {
+Navbar.select = (child) => {
     for (let k of [Navbar.research, Navbar.people, Navbar.publications, Navbar.gallery, Navbar.contact])
         k.toggleClass('selected', k === child);
     
+    
 };
 Navbar.home.pointerdown(() => window.location.reload());
-Navbar.research.pointerdown(() => {
+Navbar.research.pointerdown(async () => {
+    const separators = document.getElementsByTagName('separator');
+    const separatorLeft = separators.item(0);
+    const separatorRight = separators.item(1);
     
-    ResearchPage().init();
+    function linearGradient(opac_stop_1: [number, string], opac_stop_2: [number, string]) {
+        return `linear-gradient(90deg, rgba(0, 0, 0, ${opac_stop_1[0]}) ${opac_stop_1[1]}, rgba(0, 0, 0, ${opac_stop_2[0]}) ${opac_stop_2[1]})`
+    }
+    
+    TL.fromTo(separatorLeft, 1, {backgroundImage: linearGradient([0, '0%'], [0.25, '150%'])}, {
+        backgroundImage: linearGradient([0, '0%'], [0.5, '10%']),
+    });
+    TL.fromTo(separatorRight, 1, {backgroundImage: linearGradient([0.25, '-50%'], [0, '100%'])}, {
+        backgroundImage: linearGradient([0.5, '90%'], [0, '100%']),
+    });
 });
 Navbar.people.pointerdown(() => {
     
