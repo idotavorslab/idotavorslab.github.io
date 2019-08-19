ea422dc86a67ea87191f53e84219bf562284cb6d
giladbarnea <giladbrn@gmail.com>
2019-08-19 18:10:27 +0300

    Person class, fixed PersonExpando email bug

--- main/people/people.js
+++ main/people/people.js
@@ -1,40 +1,61 @@
 const PeoplePage = () => {
     async function init() {
         console.log('PeoplePage init');
-        const data = await fetchJson('main/people/people.json', "no-cache");
-        console.log('people data', data);
-        const people = [];
-        const { team, alumni } = data;
-        let isExpanded = false;
-        for (let [name, { image, role, cv, email }] of dict(team).items()) {
-            let person = elem({ tag: "person" });
-            person
-                .append(img({ src: `main/people/${image}` }), div({ text: name, cls: "name" }), div({ text: role, cls: "role" }));
-            person.pointerdown(async () => {
-                console.group('person.pointerdown');
-                isExpanded = !isExpanded;
-                if (!isExpanded) {
-                    elem({ query: '.expanded' }).removeClass('expanded').css({ padding: 0 });
-                    return;
+        class Person extends BetterHTMLElement {
+            *yieldIndexesBelow() {
+                for (let i = this.row + 1; i <= People.length / 4; i++) {
+                    for (let j = 0; j < 4 && i * 4 + j < People.length; j++) {
+                        console.log('i:', i, 'j:', j, `i * 4 + j:`, i * 4 + j);
+                        yield [i, j];
+                    }
                 }
+            }
+            pushPeopleBelow() {
+                for (let [i, j] of this.yieldIndexesBelow()) {
+                    People[i * 4 + j].css({ gridRow: `${i + 2}/${i + 2}` });
+                }
+            }
+            pullbackPeopleBelow() {
+                for (let [i, j] of this.yieldIndexesBelow()) {
+                    People[i * 4 + j].css({ gridRow: `${i + 1}/${i + 1}` });
+                }
+            }
+            unfocusOthers() {
+                for (let p of People) {
+                    if (p !== this) {
+                        p.addClass('unfocused');
+                    }
+                }
+            }
+            focusOthers() {
+                for (let p of People) {
+                    if (p !== this) {
+                        p.removeClass('unfocused');
+                    }
+                }
+            }
+            async collapseExpando() {
+                elem({ query: '.expanded' }).removeClass('expanded').css({ padding: 0 });
+                await wait(500);
+                PersonExpando.remove();
+            }
+            async expandExpando() {
                 if (window.innerWidth >= BP0) {
-                    let personIndex = people.indexOf(person);
-                    let personRow = int(personIndex / 4);
-                    let personIndexInRow = personIndex % 4;
-                    console.log({ personIndex, personRow, personIndexInRow, 'isExpanded (result of click)': isExpanded });
-                    for (let i = personRow + 1; i <= people.length / 4; i++) {
-                        for (let j = 0; j < 4 && i * 4 + j < people.length; j++) {
-                            console.log('i:', i, 'j:', j, `i * 4 + j:`, i * 4 + j);
-                            people[i * 4 + j].css({ gridRow: `${i + 2}/${i + 2}` });
-                        }
+                    if (this.index === undefined) {
+                        this.index = People.indexOf(this);
+                        this.row = int(this.index / 4);
+                        this.indexInRow = this.index % 4;
                     }
-                    for (let p of people) {
-                        if (p !== person) {
-                            p.toggleClass('unfocused');
-                        }
-                    }
+                    console.log({
+                        index: this.index,
+                        row: this.row,
+                        indexInRow: this.indexInRow,
+                        'IsExpanded (result of click)': IsExpanded
+                    });
+                    this.pushPeopleBelow();
+                    this.unfocusOthers();
                     let gridColumn;
-                    switch (personIndexInRow) {
+                    switch (this.indexInRow) {
                         case 0:
                             gridColumn = '1/3';
                             break;
@@ -46,24 +67,56 @@ const PeoplePage = () => {
                             gridColumn = '3/5';
                             break;
                     }
-                    let rightmostPersonIndex = 3 + (personRow % 4) * 4;
-                    console.log({ gridColumn, rightmostPersonIndex });
-                    let personExpando = div({ text: cv, cls: 'person-expando' })
-                        .append(div({ cls: 'email' }).html(`Email: <a href="mailto:${email}">${email}</a>`))
+                    console.log('PersonExpando.email', PersonExpando.email);
+                    PersonExpando
+                        .text(this.cv)
                         .css({ gridColumn });
-                    people[rightmostPersonIndex].after(personExpando);
+                    if (PersonExpando.email === undefined) {
+                        PersonExpando
+                            .cacheAppend({ email: div({ cls: 'email' }) })
+                            .email.html(`Email: <a href="mailto:${this.email}">${this.email}</a>`);
+                    }
+                    else {
+                        console.warn('PersonExpando.email is NOT undefined!');
+                    }
+                    let rightmostPersonIndex = 3 + (this.row % 4) * 4;
+                    console.log({ gridColumn, rightmostPersonIndex });
+                    People[rightmostPersonIndex].after(PersonExpando);
                     await wait(0);
-                    personExpando.addClass('expanded');
+                    PersonExpando.addClass('expanded');
                 }
                 else if (window.innerWidth >= BP1) {
                     console.warn('people.ts. person pointerdown BP1 no code');
                 }
-                console.groupEnd();
-            });
-            people.push(person);
+            }
+            constructor(image, name, role, cv, email) {
+                super({ tag: 'person' });
+                this.cv = cv;
+                this.email = email;
+                this
+                    .append(img({ src: `main/people/${image}` }), div({ text: name, cls: "name" }), div({ text: role, cls: "role" })).pointerdown(async () => {
+                    IsExpanded = !IsExpanded;
+                    if (!IsExpanded) {
+                        this.collapseExpando();
+                    }
+                    else {
+                        this.expandExpando();
+                    }
+                });
+            }
+        }
+        const data = await fetchJson('main/people/people.json', "no-cache");
+        console.log('people data', data);
+        const People = [];
+        const PersonExpando = div({ cls: 'person-expando' });
+        const { team, alumni } = data;
+        let IsExpanded = false;
+        for (let [name, { image, role, cv, email }] of dict(team).items()) {
+            let person = new Person(image, name, role, cv, email);
+            People.push(person);
         }
         const teamGrid = div({ id: "team_grid" })
-            .append(...people);
+            .append(...People);
         const alumniArr = [];
         for (let [name, { image, role, cv, email }] of dict(alumni).items()) {
             let alum = elem({ tag: "person" });
--- main/people/people.ts
+++ main/people/people.ts
@@ -2,56 +2,85 @@ const PeoplePage = () => {
     async function init() {
         console.log('PeoplePage init');
         
-        
-        const data = await fetchJson('main/people/people.json', "no-cache");
-        console.log('people data', data);
-        const people: BetterHTMLElement[] = [];
-        const {team, alumni} = data;
-        
-        // **  Team
-        let isExpanded = false;
-        for (let [name, {image, role, cv, email}] of dict(team).items()) {
-            let person = elem({tag: "person"});
-            person
-                .append(
-                    img({src: `main/people/${image}`}),
-                    div({text: name, cls: "name"}),
-                    div({text: role, cls: "role"}),
-                );
-            person.pointerdown(async () => {
-                console.group('person.pointerdown');
-                isExpanded = !isExpanded;
-                if (!isExpanded) {
-                    elem({query: '.expanded'}).removeClass('expanded').css({padding: 0});
-                    
-                    return;
+        class Person extends BetterHTMLElement {
+            index: number;
+            row: number;
+            indexInRow: number;
+            cv: string;
+            email: string;
+            
+            * yieldIndexesBelow() {
+                
+                for (let i = this.row + 1; i <= People.length / 4; i++) {
+                    for (let j = 0; j < 4 && i * 4 + j < People.length; j++) {
+                        
+                        console.log('i:', i, 'j:', j, `i * 4 + j:`, i * 4 + j);
+                        // 4,5,6,7                  3/3     (go over row and increment gridRow)
+                        yield [i, j];
+                        
+                    }
                 }
+            }
+            
+            pushPeopleBelow() {
+                for (let [i, j] of this.yieldIndexesBelow()) {
+                    People[i * 4 + j].css({gridRow: `${i + 2}/${i + 2}`});
+                }
+                
+            }
+            
+            pullbackPeopleBelow() {
+                for (let [i, j] of this.yieldIndexesBelow()) {
+                    People[i * 4 + j].css({gridRow: `${i + 1}/${i + 1}`});
+                }
+            }
+            
+            unfocusOthers() {
+                for (let p of People) {
+                    if (p !== this) {
+                        p.addClass('unfocused')
+                    }
+                }
+            }
+            
+            focusOthers() {
+                for (let p of People) {
+                    if (p !== this) {
+                        p.removeClass('unfocused')
+                    }
+                }
+            }
+            
+            async collapseExpando() {
+                elem({query: '.expanded'}).removeClass('expanded').css({padding: 0});
+                // elem({query: '.expanded'}).removeClass('expanded');
+                
+                await wait(500); // *  DEP: people.sass padding transition
+                PersonExpando.remove();
+                // this.pullbackPeopleBelow();
+                // this.focusOthers();
+            }
+            
+            async expandExpando() {
                 if (window.innerWidth >= BP0) {
-                    let personIndex = people.indexOf(person);
+                    if (this.index === undefined) {
+                        this.index = People.indexOf(this);
+                        this.row = int(this.index / 4);
+                        this.indexInRow = this.index % 4;
+                    }
                     // eg 0
-                    let personRow = int(personIndex / 4);
-                    let personIndexInRow = personIndex % 4;
-                    console.log({personIndex, personRow, personIndexInRow, 'isExpanded (result of click)': isExpanded});
-                    // i=1; i<=1 (from next row and on)
-                    for (let i = personRow + 1; i <= people.length / 4; i++) {
-                        for (let j = 0; j < 4 && i * 4 + j < people.length; j++) {
-                            
-                            console.log('i:', i, 'j:', j, `i * 4 + j:`, i * 4 + j);
-                            // 4,5,6,7                  3/3     (go over row and increment gridRow)
-                            people[i * 4 + j].css({gridRow: `${i + 2}/${i + 2}`});
-                            
-                            
-                        }
-                    }
-                    // unfocus all persons
-                    for (let p of people) {
-                        if (p !== person) {
-                            p.toggleClass('unfocused')
-                        }
-                    }
+                    console.log({
+                        index: this.index,
+                        row: this.row,
+                        indexInRow: this.indexInRow,
+                        'IsExpanded (result of click)': IsExpanded
+                    });
+                    this.pushPeopleBelow();
+                    this.unfocusOthers();
+                    
                     // 3, 7, 11.. last person in each row
                     let gridColumn;
-                    switch (personIndexInRow) {
+                    switch (this.indexInRow) {
                         case 0:
                             gridColumn = '1/3';
                             break;
@@ -63,24 +92,68 @@ const PeoplePage = () => {
                             gridColumn = '3/5';
                             break;
                     }
-                    let rightmostPersonIndex = 3 + (personRow % 4) * 4;
-                    console.log({gridColumn, rightmostPersonIndex});
-                    let personExpando = div({text: cv, cls: 'person-expando'})
-                        .append(div({cls: 'email'}).html(`Email: <a href="mailto:${email}">${email}</a>`))
+                    // PersonExpando.text(this.cv).css({gridColumn}).email.html(`Email: <a href="mailto:${this.email}">${this.email}</a>`);
+                    console.log('PersonExpando.email', PersonExpando.email);
+                    PersonExpando
+                        .text(this.cv)
                         .css({gridColumn});
-                    people[rightmostPersonIndex].after(personExpando);
+                    if (PersonExpando.email === undefined) {
+                        PersonExpando
+                            .cacheAppend({email: div({cls: 'email'})})
+                            .email.html(`Email: <a href="mailto:${this.email}">${this.email}</a>`);
+                    } else {
+                        console.warn('PersonExpando.email is NOT undefined!');
+                    }
+                    let rightmostPersonIndex = 3 + (this.row % 4) * 4;
+                    console.log({gridColumn, rightmostPersonIndex});
+                    People[rightmostPersonIndex].after(PersonExpando);
+                    
                     await wait(0);
-                    personExpando.addClass('expanded');
+                    PersonExpando.addClass('expanded');
                 } else if (window.innerWidth >= BP1) {
                     console.warn('people.ts. person pointerdown BP1 no code');
                 }
-                console.groupEnd();
-            });
-            people.push(person);
+                
+            }
+            
+            constructor(image: string, name: string, role: string, cv: string, email: string) {
+                super({tag: 'person'});
+                this.cv = cv;
+                this.email = email;
+                this
+                    .append(
+                        img({src: `main/people/${image}`}),
+                        div({text: name, cls: "name"}),
+                        div({text: role, cls: "role"}),
+                    ).pointerdown(async () => {
+                    
+                    IsExpanded = !IsExpanded;
+                    if (!IsExpanded) {
+                        this.collapseExpando();
+                    } else {
+                        this.expandExpando();
+                    }
+                })
+            }
+        }
+        
+        const data = await fetchJson('main/people/people.json', "no-cache");
+        console.log('people data', data);
+        const People: BetterHTMLElement[] = [];
+        // const PersonExpando: any & { email: Div } = div({cls: 'person-expando'}).cacheAppend({email: div({cls: 'email'})});
+        const PersonExpando: any & { email: Div } = div({cls: 'person-expando'});
+        
+        const {team, alumni} = data;
+        
+        // **  Team
+        let IsExpanded = false;
+        for (let [name, {image, role, cv, email}] of dict(team).items()) {
+            let person = new Person(image, name, role, cv, email);
+            People.push(person);
         }
         const teamGrid = div({id: "team_grid"})
             .append(
-                ...people
+                ...People
             );
         
         // **  Alumni
