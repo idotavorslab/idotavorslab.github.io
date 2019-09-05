7ed7d84d270977ab76602ff7dfa3b30e444f5421
giladbarnea <giladbrn@gmail.com>
2019-09-05 16:53:01 +0300

    HomePage.init()

--- main/home/home.ts
+++ main/home/home.ts
@@ -12,80 +12,6 @@ const HomePage = () => {
     });
     const newsChildren: HTMLElement[] = newsElem.children().map(c => c.e);
     
-    /*class CarouselItem {
-        title: string;
-        image: string;
-        content: string;
-        
-        constructor(title: string, image: string, content: string) {
-            this.title = title;
-            this.image = image;
-            this.content = content;
-        }
-    }
-    
-    class Carousel extends BetterHTMLElement {
-        left: BetterHTMLElement;
-        right: BetterHTMLElement;
-        headline: BetterHTMLElement;
-        content: BetterHTMLElement;
-        items: CarouselItem[];
-        image: BetterHTMLElement;
-        currentIndex: number;
-        
-        constructor(elemOptions: ElemOptions, items: CarouselItem[]) {
-            super(elemOptions);
-            this.items = items;
-            this.currentIndex = 0;
-            this._switch();
-            this.headline.on({
-                pointerdown: () => {
-                    console.log('headline pointerdown');
-                    ResearchPage().init(this.currentIndex);
-                },
-                
-            });
-            
-            this.left.pointerdown(() => {
-                this._switch("left");
-                
-                
-            });
-            this.right.pointerdown(() => {
-                this._switch("right");
-            });
-        }
-        
-        private _switch(to?: 'right' | 'left') {
-            TL.to([this.content.e, this.headline.e], 0.1, {opacity: 0});
-            TL.fromTo(this.e, 0.2, {opacity: 1}, {
-                opacity: 0.5,
-                ease: Power2.easeIn,
-                onComplete: () => {
-                    if (to === "right")
-                        this.currentIndex++;
-                    else if (to === "left")
-                        this.currentIndex--;
-                    else if (to !== undefined)
-                        throw new TypeError(`_switch illegal 'to' param, got: ${to}`);
-                    if (this.currentIndex == -1)
-                        this.currentIndex = this.items.length - 1;
-                    else if (this.currentIndex == this.items.length)
-                        this.currentIndex = 0;
-                    this.content.text(this.items[this.currentIndex].content);
-                    this.headline.text(this.items[this.currentIndex].title);
-                    this.image.css({backgroundImage: `linear-gradient(90deg, rgba(255, 255, 255,1) 40%, rgba(255,255,255,0)), url("main/research/${this.items[this.currentIndex].image}")`});
-                    TL.to([this.content.e, this.headline.e], 0.3, {opacity: 1});
-                    TL.to(this.e, 1, {opacity: 1})
-                }
-            });
-            
-            
-        }
-        
-        
-    }
-    */
     class NewsData {
         readonly data: TNewsDataItem[];
         private _selected: TNewsDataItem;
@@ -216,7 +142,7 @@ const HomePage = () => {
     
     return {init}
 };
-// HomePage().init();
+HomePage().init();
 
 
 
