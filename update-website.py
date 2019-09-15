5848deb5fb909ab79421ff1cfac58ca19b123e86
giladbarnea <giladbrn@gmail.com>
2019-09-15 16:53:00 +0300

    update-website.py, clone-and-replace.sh

--- update-website.py
+++ update-website.py
@@ -1,11 +1,38 @@
 import re
+import os
+import sys
+ok = raw_input('You are about to UPDATE the actual website with changes you made, continue? y/n\t')
+if ok.lower() != 'y':
+    print 'Exiting'
+    sys.exit()
+
+
 change = raw_input('What was changed? (no quotes)\t')
 trynum = 1
 
-while not change or not re.findall(r'\w',change):
-    print "! user input was empty or had no letter, try again (try number: %d)" % trynum
+while not change or not re.findall(r'\w', change):
+    print "! user input was empty or had no letters, try again (try number: %d)\t" % trynum
     change = raw_input('Bad input, what was changed?')
     trynum += 1
-import os
 
+
+"""
+cd /a/home/cc/tree/taucc/students/lifesci/idotavor/public_html
+git clone https://github.com/idotavorslab/idotavorslab.github.io.git __tmp
+rm -rf __tmp/.git
+cp -v -r __tmp/* .
+rm -rf __tmp
+find -iname "*.py" -type f -exec rm '{}' ';'
+find -iname "*.sh" -type f -exec rm '{}' ';'
+find -iname "*.sass" -type f -exec rm '{}' ';'
+find -iname "*.map" -type f -exec rm '{}' ';'
+find -iname "*.ts" -type f -exec rm '{}' ';'
+find -iname "*.md" -type f -exec rm '{}' ';'
+find -iname "*.zip" -type f -exec rm '{}' ';'
+exit
+"""
+print '\nPushing to GitHub...\n'
 os.system('git add . && git commit -a -m "' + change + '" && git push')
+print '\nSuccess pushing to GitHub, now copying files through SSH...\n'
+print 'sh /a/home/cc/tree/taucc/students/lifesci/idotavor/public_html/clone-and-replace.sh'
+os.system('ssh idotavor@gp.tau.ac.il')
