2232336dc532a3654e49912b5046c464a56ae1be
giladbarnea <giladbrn@gmail.com>
2019-09-22 16:20:17 +0300

    optimize-images.py writes png

--- optimize-images.py
+++ optimize-images.py
@@ -1,17 +1,19 @@
-from typing import List, Any
+from typing import List
 
 from PIL import Image
 from pathlib import Path
 import sys
 import os
+import png
 
 gallery_path = Path('main/gallery')
 opt_path = (gallery_path / 'opt')
 if not opt_path.is_dir():
     opt_path.mkdir()
 
-doubles = set()
+duplicates = set()
 
+# none of the following 3 lists have duplicates in either list
 gallery_images: List[Path] = [e for e in gallery_path.iterdir() if e.suffix.lower() in ['.jpg', '.jpeg', '.png']]
 jpgs: List[Path] = []
 pngs: List[Path] = []
@@ -21,23 +23,52 @@ def populate_jpgs_pngs():
     for img in gallery_images:
         if img.suffix.lower() in ['.jpg', '.jpeg']:
             jpgs.append(img)
-        elif img.suffix.lower() == 'png':
+        elif img.suffix.lower() == '.png':
             pngs.append(img)
 
 
-def separate_doubles():
+def separate_duplicates():
     for i_img in gallery_images:
         for j__img in gallery_images:
             if i_img != j__img and i_img.stem.lower() == j__img.stem.lower():
-                doubles.add(i_img)
-                doubles.add(j__img)
+                duplicates.add(i_img)
+                duplicates.add(j__img)
 
-    for double in doubles:
-        gallery_images.remove(double)
+    for dup in duplicates:
+        gallery_images.remove(dup)
 
 
-separate_doubles()
-print(doubles)
+def interlace_pngs():
+    for png_path in pngs:
+        if '23-no-il' not in png_path.stem:
+            continue
+        png_img: Image.Image = Image.open(png_path)
+        # interlaced = bool(png_img.info.get('interlace'))
+        reader: png.Reader = png.Reader(bytes=png_path.read_bytes())
+        reader.read()
+        if reader.interlace:
+            reduce_filesize(reader)
+        else:
+            write_path = opt_path / png_path.name
+            with open(write_path, 'w+b') as f:
+                writer = png.Writer(width=reader.width,
+                                    height=reader.height,
+                                    bitdepth=reader.bitdepth,
+                                    compression=reader.compression,
+                                    greyscale=False,
+                                    interlace=True
+                                    )
+                direct = list(reader.asDirect()[2])
+                writer.write(f, direct)
+
+
+def reduce_filesize(png_reader: png.Reader):
+    pass
+
+
+separate_duplicates()
+print(duplicates)
+populate_jpgs_pngs()
 
 
 def foo():
@@ -47,7 +78,8 @@ def foo():
     # if ext in ['jpg', 'jpeg']:
     #     jpg = Image.open(pathlib.)
 
-# print(doubles)
+
+interlace_pngs()
 # no_il = Image.open('main/gallery/23-gimp-no-interlace.png')
 # il = Image.open('main/gallery/23-gimp-interlace.png')
 # jpg = Image.open('main/gallery/23-gimp-interlace-q95-opt-prog.jpg')
