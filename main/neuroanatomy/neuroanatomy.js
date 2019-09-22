41dc746b455ed9fefefc863542c4ed67d545ea5b
giladbarnea <giladbrn@gmail.com>
2019-09-22 19:23:05 +0300

    neuroanatomy.json now a list of objects

--- main/neuroanatomy/neuroanatomy.js
+++ main/neuroanatomy/neuroanatomy.js
@@ -2,20 +2,21 @@ const NeuroanatomyPage = () => {
     async function init() {
         const data = await fetchJson('main/neuroanatomy/neuroanatomy.json', "no-cache");
         const brains = [];
-        for (let [title, url] of Object.entries(data)) {
-            let brain = div({ cls: 'brain' }).append(elem({ tag: 'h1' }).text(title), div({ cls: 'sketchfab-embed-wrapper' }).html(wrapSketch(title, url)));
+        for (let { title, text, link } of data) {
+            console.log(JSON.parstr({ title, text, link }));
+            let brain = div({ cls: 'brain' }).append(elem({ tag: 'h1' }).text(title), div({ cls: 'sketchfab-embed-wrapper' }).html(wrapSketch(title, link)));
             brains.push(brain);
         }
         Home.empty().append(...brains);
     }
-    function wrapSketch(title, url) {
-        if (url.match(/sketchfab\.com/) === null) {
-            return console.error("neuroanatomy.json: url didn't include 'sketchfab.com'");
+    function wrapSketch(title, link) {
+        if (link.match(/sketchfab\.com/) === null) {
+            return console.error("neuroanatomy.json: link didn't include 'sketchfab.com'");
         }
-        if (url.endsWith('/')) {
-            url.slice(0, url.length - 1);
+        if (link.endsWith('/')) {
+            link.slice(0, link.length - 1);
         }
-        return `<iframe src="${url}/embed?autospin=0.2&amp;preload=1" title="${title}" width="640" height="480" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>`;
+        return `<iframe src="${link}/embed?autospin=0.2&amp;preload=1" title="${title}" width="640" height="480" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>`;
     }
     return { init };
 };
--- main/neuroanatomy/neuroanatomy.ts
+++ main/neuroanatomy/neuroanatomy.ts
@@ -2,29 +2,30 @@ const NeuroanatomyPage = () => {
     
     
     async function init() {
-        const data = await fetchJson('main/neuroanatomy/neuroanatomy.json', "no-cache");
+        const data: { title: string, text: string, link: string }[] = await fetchJson('main/neuroanatomy/neuroanatomy.json', "no-cache");
         const brains = [];
-        for (let [title, url] of Object.entries(<TMap<string>>data)) {
+        for (let {title, text, link} of data) {
+            console.log(JSON.parstr({title, text, link}));
             let brain = div({cls: 'brain'}).append(
                 elem({tag: 'h1'}).text(title),
-                div({cls: 'sketchfab-embed-wrapper'}).html(wrapSketch(title, url))
+                div({cls: 'sketchfab-embed-wrapper'}).html(wrapSketch(title, link))
             );
             brains.push(brain);
         }
         Home.empty().append(...brains)
     }
     
-    function wrapSketch(title: string, url: string): string {
-        if (url.match(/sketchfab\.com/) === null) {
+    function wrapSketch(title: string, link: string): string {
+        if (link.match(/sketchfab\.com/) === null) {
             // @ts-ignore
-            return console.error("neuroanatomy.json: url didn't include 'sketchfab.com'")
+            return console.error("neuroanatomy.json: link didn't include 'sketchfab.com'")
         }
         
-        if (url.endsWith('/')) {
-            url.slice(0, url.length - 1);
+        if (link.endsWith('/')) {
+            link.slice(0, link.length - 1);
         }
         
-        return `<iframe src="${url}/embed?autospin=0.2&amp;preload=1" title="${title}" width="640" height="480" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>`
+        return `<iframe src="${link}/embed?autospin=0.2&amp;preload=1" title="${title}" width="640" height="480" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>`
     }
     
     return {init}
