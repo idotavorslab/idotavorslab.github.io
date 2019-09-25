8eb5b672321d86f5e74648ebea7862a8d03d4b03
giladbarnea <giladbrn@gmail.com>
2019-09-25 20:52:05 +0300

    contact form embedded in contact

--- main/contact/contact.js
+++ main/contact/contact.js
@@ -2,23 +2,33 @@ const ContactPage = () => {
     async function init() {
         const data = await fetchJson("main/contact/contact.json");
         const visit = div({ cls: 'visit' })
-            .append(elem({ tag: 'h1', text: 'Visit' }), img({ src: 'main/contact/Home_font_awesome.svg' }), paragraph({ cls: 'subtitle', text: 'Address' }), anchor({ href: data.visit.link, text: data.visit.address }).target("_blank"));
+            .append(elem({ tag: 'h1', text: 'Visit' }), img({ src: 'main/contact/home.svg' }), paragraph({ cls: 'subtitle', text: 'Address' }), anchor({ href: data.visit.link, text: data.visit.address }).target("_blank"));
         const call = div({ cls: 'call' })
-            .append(elem({ tag: 'h1', text: 'Call' }), img({ src: 'main/contact/Phone_font_awesome.svg' }), paragraph({ cls: 'subtitle', text: data.call.hours }), anchor({ text: data.call.phone, href: `tel:${data.call.phone}` }).target("_blank"));
+            .append(elem({ tag: 'h1', text: 'Call' }), img({ src: 'main/contact/phone.svg' }), paragraph({ cls: 'subtitle', text: data.call.hours }), anchor({ text: data.call.phone, href: `tel:${data.call.phone}` }).target("_blank"));
         const email = div({ cls: 'email' })
-            .append(elem({ tag: 'h1', text: 'Email' }), img({ src: 'main/contact/Envelope_alt_font_awesome.svg' }), anchor({
+            .append(elem({ tag: 'h1', text: 'Email' }), img({ src: 'main/contact/envelope.svg' }), anchor({
             text: data.email,
             href: `mailto:${data.email}`
         }).target("_blank"));
         const grid = div({ id: 'contact_grid' })
             .append(visit, call, email);
-        Home.empty().class('contact-page').append(grid, elem({ tag: 'iframe' })
-            .id('contact_iframe')
+        const form = elem({ tag: 'iframe', text: "Loading" })
+            .id('contact_form')
+            .attr({
+            frameborder: "0",
+            allowfullscreen: "",
+            marginheight: "0",
+            marginwidth: "0",
+            src: data.form
+        });
+        let map = elem({ tag: 'iframe' })
+            .id('contact_map')
             .attr({
             frameborder: "0",
             allowfullscreen: "",
             src: data.map
-        }));
+        });
+        Home.empty().class('contact-page').append(grid, form, map);
     }
     return { init };
 };
--- main/contact/contact.ts
+++ main/contact/contact.ts
@@ -4,41 +4,57 @@ const ContactPage = () => {
         const visit = div({cls: 'visit'})
             .append(
                 elem({tag: 'h1', text: 'Visit'}),
-                img({src: 'main/contact/Home_font_awesome.svg'}),
+                img({src: 'main/contact/home.svg'}),
                 paragraph({cls: 'subtitle', text: 'Address'}),
                 anchor({href: data.visit.link, text: data.visit.address}).target("_blank")
             );
+        
         const call = div({cls: 'call'})
+            
             .append(
                 elem({tag: 'h1', text: 'Call'}),
-                img({src: 'main/contact/Phone_font_awesome.svg'}),
+                img({src: 'main/contact/phone.svg'}),
                 paragraph({cls: 'subtitle', text: data.call.hours}),
                 anchor({text: data.call.phone, href: `tel:${data.call.phone}`}).target("_blank")
             );
+        
         const email = div({cls: 'email'})
             .append(
                 elem({tag: 'h1', text: 'Email'}),
-                img({src: 'main/contact/Envelope_alt_font_awesome.svg'}),
+                img({src: 'main/contact/envelope.svg'}),
                 anchor({
                     text: data.email,
                     href: `mailto:${data.email}`
                 }).target("_blank")
             );
+        
         const grid = div({id: 'contact_grid'})
             .append(
                 visit,
                 call,
                 email
             );
+        
+        const form = elem({tag: 'iframe', text: "Loading"})
+            .id('contact_form')
+            .attr({
+                frameborder: "0",
+                allowfullscreen: "",
+                marginheight: "0",
+                marginwidth: "0",
+                src: data.form
+            });
+        let map = elem({tag: 'iframe'})
+            .id('contact_map')
+            .attr({
+                frameborder: "0",
+                allowfullscreen: "",
+                src: data.map
+            });
         Home.empty().class('contact-page').append(
             grid,
-            elem({tag: 'iframe'})
-                .id('contact_iframe')
-                .attr({
-                    frameborder: "0",
-                    allowfullscreen: "",
-                    src: data.map
-                })
+            form,
+            map
         )
     }
     
