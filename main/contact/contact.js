f81449db80c033eb2346f69529bb3ef684befaea
giladbarnea <giladbrn@gmail.com>
2019-10-02 18:20:49 +0300

    contact svgs inline

--- main/contact/contact.js
+++ main/contact/contact.js
@@ -1,12 +1,18 @@
 const ContactPage = () => {
     async function init() {
+        function htmlToElement(html) {
+            const template = document.createElement('template');
+            html = html.trim();
+            template.innerHTML = html;
+            return template.content.firstChild;
+        }
         const data = await fetchJson("main/contact/contact.json");
         const visit = div({ cls: 'visit' })
-            .append(elem({ tag: 'h1', text: 'Visit' }), div().css({ backgroundImage: `url(main/contact/${data.visit.icon}` }), paragraph({ cls: 'subtitle', text: 'Address' }), anchor({ href: data.visit.link, text: data.visit.address }).target("_blank"));
+            .append(elem({ tag: 'h1', text: 'Visit' }), htmlToElement(await fetchText(`main/contact/${data.visit.icon}`)), paragraph({ cls: 'subtitle', text: 'Address' }), anchor({ href: data.visit.link, text: data.visit.address }).target("_blank"));
         const call = div({ cls: 'call' })
-            .append(elem({ tag: 'h1', text: 'Call' }), div().css({ backgroundImage: `url(main/contact/${data.call.icon}` }), paragraph({ cls: 'subtitle', text: data.call.hours }), anchor({ text: data.call.phone, href: `tel:${data.call.phone}` }).target("_blank"));
+            .append(elem({ tag: 'h1', text: 'Call' }), htmlToElement(await fetchText(`main/contact/${data.call.icon}`)), paragraph({ cls: 'subtitle', text: data.call.hours }), anchor({ text: data.call.phone, href: `tel:${data.call.phone}` }).target("_blank"));
         const email = div({ cls: 'email' })
-            .append(elem({ tag: 'h1', text: 'Email' }), div().css({ backgroundImage: `url(main/contact/${data.email.icon}` }), anchor({ text: data.email.address, href: `mailto:${data.email.address}` }).target("_blank"));
+            .append(elem({ tag: 'h1', text: 'Email' }), htmlToElement(await fetchText(`main/contact/${data.email.icon}`)), anchor({ text: data.email.address, href: `mailto:${data.email.address}` }).target("_blank"));
         const grid = div({ id: 'contact_grid' })
             .append(visit, call, email);
         const form = elem({ tag: 'iframe', text: "Loading" })
