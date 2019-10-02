c19b10b0c1b1c6760d0ba413f3b0ea9eda0d3d9b
giladbarnea <giladbrn@gmail.com>
2019-10-02 18:21:26 +0300

    contact svgs inline

--- main/contact/contact.ts
+++ main/contact/contact.ts
@@ -1,20 +1,20 @@
 const ContactPage = () => {
     async function init() {
-        /*function htmlToElement(html) {
+        function htmlToElement(html) {
             const template = document.createElement('template');
             html = html.trim(); // Never return a text node of whitespace as the result
             template.innerHTML = html;
             return template.content.firstChild;
         }
-        */
+        
         
         // let svg = await fetchText("main/contact/home-simple.svg");
         const data = await fetchJson("main/contact/contact.json");
         const visit = div({cls: 'visit'})
             .append(
                 elem({tag: 'h1', text: 'Visit'}),
-                // htmlToElement(svg),
-                div().css({backgroundImage: `url(main/contact/${data.visit.icon}`}),
+                htmlToElement(await fetchText(`main/contact/${data.visit.icon}`)),
+                // div().css({backgroundImage: `url(main/contact/${data.visit.icon}`}),
                 paragraph({cls: 'subtitle', text: 'Address'}),
                 anchor({href: data.visit.link, text: data.visit.address}).target("_blank")
             );
@@ -23,7 +23,8 @@ const ContactPage = () => {
             
             .append(
                 elem({tag: 'h1', text: 'Call'}),
-                div().css({backgroundImage: `url(main/contact/${data.call.icon}`}),
+                // div().css({backgroundImage: `url(main/contact/${data.call.icon}`}),
+                htmlToElement(await fetchText(`main/contact/${data.call.icon}`)),
                 paragraph({cls: 'subtitle', text: data.call.hours}),
                 anchor({text: data.call.phone, href: `tel:${data.call.phone}`}).target("_blank")
             );
@@ -31,7 +32,8 @@ const ContactPage = () => {
         const email = div({cls: 'email'})
             .append(
                 elem({tag: 'h1', text: 'Email'}),
-                div().css({backgroundImage: `url(main/contact/${data.email.icon}`}),
+                // div().css({backgroundImage: `url(main/contact/${data.email.icon}`}),
+                htmlToElement(await fetchText(`main/contact/${data.email.icon}`)),
                 anchor({text: data.email.address, href: `mailto:${data.email.address}`}).target("_blank")
             );
         
